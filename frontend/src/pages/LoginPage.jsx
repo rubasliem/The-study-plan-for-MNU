23170596f6952098578d14923d5aed154eca9706
ruba sleim <rubasleim@gmail.com>
2026-09-09 04:10:12 +0300

    Fix: enlarge inner white login circle to 800px

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -141,8 +141,8 @@ const LoginPage = () => {
                 {/* Inner White Login Circle */}
                 <div 
                     style={{
-                        width: '650px',
-                        height: '650px',
+                        width: '800px',
+                        height: '800px',
                         maxWidth: '92%',
                         maxHeight: '92%',
                         borderRadius: '50%',
